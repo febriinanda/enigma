c57f0163bfd784305a157f6e7a9685287953bd93
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-01-02 00:21:22 +0700

    #time 3h #comment Client UI, with history table, notification, and confirm button

--- query.js
+++ query.js
@@ -49,7 +49,7 @@ router.get('/byInternet', function(req, res){
 })
 
 router.get('/tagihan', function(req, res){
-  var sql = "SELECT SUM(harga) as total FROM tagihan WHERE NCLI = ? and STATUS = 0";
+  var sql = "SELECT SUM(harga) as total FROM tagihan WHERE NCLI = ? and STATUS != 1";
   var id = req.query.ncli;
   connection.query(sql,[id], function(err, rows, fields){
     if(err) throw err;
@@ -60,6 +60,33 @@ router.get('/tagihan', function(req, res){
     }else{
       res.status(500).json({ error: 'Data not found dude!' });
     }
+  });
+})
+
+router.get('/tagihanUpdate', function(req, res){
+  console.log("/put tagihan", req);
+  var sql = "update tagihan set status = ?, tgl_bayar = NOW() where NCLI = ? and status = ?";
+  var id = req.query.id;
+  var status = req.query.status;
+  var oldStatus = req.query.oldStatus;
+  connection.query(sql,[status, id, oldStatus], function(err, rows, fields){
+    if(err) throw err;
+
+    res.json({"result":"Updated"});
+  });
+})
+
+router.get('/history', function(req, res){
+  var sql = "select a.NCLI, a.NAMA, sum(B.HARGA) AS TOTAL, b.TGL_BAYAR, b.STATUS from tagihan b left join pelanggan a on a.NCLI = b.NCLI where b.status != 0 group by a.NCLI, b.STATUS"
+  connection.query(sql, function(err, rows, fields){
+    if(err) throw err;
+
+    if(rows.length > 0){
+        res.json(rows);
+    }else{
+      res.status(500).json({ error: 'Data not found dude!' });
+    }
+
   });
 })
 
